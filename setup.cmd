6c428933d3a14072ad12567813f90a56447f30c3
Dirk Arnez <smalldirkalex@gmail.com>
2020-10-21 15:49:23 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -8,6 +8,9 @@ echo.Unzipping PortableGit...
 echo.Unzipping node...
 "C:\Program Files\7-Zip\7z.exe" x node-v12.19.0-win-x64.zip -o%USERPROFILE%\Downloads\node-v12.19.0-win-x64
 
+echo.Copying Launchers...
+copy Launchers\Code.cmd %USERPROFILE%\Downloads\VSCode-win32-x64-1.50.1
+
 echo.Setting up git config...
 SET PATH=%USERPROFILE%\Downloads\PortableGit\bin;
 
@@ -15,8 +18,5 @@ cd /d %USERPROFILE%
 
 git config --global user.name "dirkarnez"
 git config --global user.email "smalldirkalex@gmail.com"
-
-echo.Copying Launchers...
-copy Launchers\Code.cmd %USERPROFILE%\Downloads\VSCode-win32-x64-1.50.1
 
 pause
