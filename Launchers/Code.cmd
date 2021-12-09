b0ac54736369598e768477ab48c90efca8e5c0a4
Dirk Arnez <smalldirkalex@gmail.com>
2021-12-09 15:00:44 +0800

    - update Launchers/Code.cmd

--- Launchers/Code.cmd
+++ Launchers/Code.cmd
@@ -1,5 +1,5 @@
 @echo off
-SET PATH=%PATH%;%USERPROFILE%\Downloads\LLVM-13.0.0-win64\bin;%USERPROFILE%\Downloads\node-v12.19.0-win-x64;%USERPROFILE%\Downloads\PortableGit\bin;%USERPROFILE%\Downloads\go\bin;%USERPROFILE%\Downloads\mingw64\bin;
+SET PATH=%PATH%;%USERPROFILE%\Downloads\LLVM-13.0.0-win64\bin;%USERPROFILE%\Downloads\node-v16.13.1-win-x64;%USERPROFILE%\Downloads\PortableGit\bin;%USERPROFILE%\Downloads\go\bin;%USERPROFILE%\Downloads\mingw64\bin;
 
 REM set EXTENSION_PATH="%~dp0extensions"
 REM set USER_DATA_PATH="%~dp0user_data"
