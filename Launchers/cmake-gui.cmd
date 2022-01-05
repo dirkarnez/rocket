74e220564944a1afa6b7be82084f2b109a3ca49e
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-05 17:45:46 +0800

    - upload files

--- Launchers/cmake-gui.cmd
+++ Launchers/cmake-gui.cmd
@@ -1,3 +1,3 @@
 @echo off
-SET PATH=%PATH%;%USERPROFILE%\Downloads\PortableGit\bin;%USERPROFILE%\Downloads\LLVM-13.0.0-woa64\bin;%USERPROFILE%\Downloads\mingw64\bin;
+SET PATH=%PATH%;%USERPROFILE%\Downloads;%USERPROFILE%\Downloads\PortableGit\bin;%USERPROFILE%\Downloads\LLVM-13.0.0-woa64\bin;%USERPROFILE%\Downloads\mingw64;%USERPROFILE%\Downloads\mingw64\bin;
 start cmake-gui.exe
--- Launchers/git-cmd.cmd
+++ Launchers/git-cmd.cmd
@@ -1,4 +1,4 @@
 @echo off
-SET PATH=%PATH%;%USERPROFILE%\Downloads\LLVM-13.0.0-win64\bin;%USERPROFILE%\Downloads\node-v12.19.0-win-x64;%USERPROFILE%\Downloads\PortableGit\bin;%USERPROFILE%\Downloads\go\bin;%USERPROFILE%\Downloads\mingw64\bin;
+SET PATH=%PATH%;C:\Users\19081126D\Downloads\LLVM-13.0.0-win64\bin;C:\Users\19081126D\Downloads\node-v16.13.1-win-x64;C:\Users\19081126D\Downloads\PortableGit\bin;C:\Users\19081126D\Downloads\go\bin;C:\Users\19081126D\Downloads\mingw64\bin;
 
 start git-cmd.exe
