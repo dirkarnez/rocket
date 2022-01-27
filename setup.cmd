b5df5d6ef272aebbcfe1b7687dc371c1f38b1166
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-27 23:10:06 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -24,7 +24,7 @@ echo Unzipping mingw64-win32...
 ren "%DOWNLOADS_DIR%\mingw64" "mingw64-win32"
 
 echo Unzipping cmake...
-"C:\Program Files\7-Zip\7z.exe" x cmake-3.22.0-rc1-windows-x86_64.zip -o%DOWNLOADS_DIR%
+"C:\Program Files\7-Zip\7z.exe" x cmake-3.22.2-windows-x86_64.zip -o%DOWNLOADS_DIR%
 
 echo Unzipping arduino...
 "C:\Program Files\7-Zip\7z.exe" x arduino-1.8.19-windows.zip -o%DOWNLOADS_DIR%
@@ -42,7 +42,7 @@ cd ..
 
 echo Copying Launchers...
 copy Launchers\Code.cmd %DOWNLOADS_DIR%\VSCode-win32-x64-1.62.3
-copy Launchers\cmake-gui.cmd %DOWNLOADS_DIR%\cmake-3.22.0-rc1-windows-x86_64\bin
+copy Launchers\cmake-gui.cmd %DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin
 copy Launchers\git-cmd.cmd %DOWNLOADS_DIR%\PortableGit
 copy gopath\src\github.com\dirkarnez\serial-locate\serial-locate.exe %DOWNLOADS_DIR%
 copy Software\staticserver.exe %DOWNLOADS_DIR%
