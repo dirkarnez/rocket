8cb59e51cc12731055c0801a8c7ccfe9b599a79f
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-27 23:11:41 +0800

    - rename cmake-gui.cmd to cmake-gui-mingw64-win32.cmd

---
+++ Launchers/cmake-gui-mingw64-win32.cmd
@@ -0,0 +1,6 @@
+@echo off
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+SET PATH=%PATH%;%DOWNLOADS_DIR%;%DOWNLOADS_DIR%\PortableGit\bin;%DOWNLOADS_DIR%\LLVM-13.0.0-woa64\bin;%DOWNLOADS_DIR%\mingw64-win32;%DOWNLOADS_DIR%\mingw64-win32\bin;
+
+start cmake-gui.exe
