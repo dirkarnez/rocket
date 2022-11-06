602d4099b73b7376cc1c3fcf786d63d79cf68848
Dirk Arnez <smalldirkalex@gmail.com>
2022-11-06 22:13:04 +0800

    - update Launchers/Code.cmd

--- Launchers/Code.cmd
+++ Launchers/Code.cmd
@@ -1,7 +1,14 @@
 @echo off
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 
-SET PATH=%PATH%;%DOWNLOADS_DIR%;%DOWNLOADS_DIR%\LLVM-13.0.0-win64\bin;%DOWNLOADS_DIR%\node-v16.13.1-win-x64;%DOWNLOADS_DIR%\PortableGit\bin;%DOWNLOADS_DIR%\go\bin;%DOWNLOADS_DIR%\mingw64;%DOWNLOADS_DIR%\mingw64\bin;
+SET PATH=^
+%DOWNLOADS_DIR%\LLVM-13.0.0-win64\bin;^
+%DOWNLOADS_DIR%\node-v16.13.1-win-x64;^
+%DOWNLOADS_DIR%\go\bin;^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
+%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;
 
 REM set EXTENSION_PATH="%~dp0extensions"
 REM set USER_DATA_PATH="%~dp0user_data"
