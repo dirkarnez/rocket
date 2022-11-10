99298ed7cd7659d1016d7f2f51944d454dde0706
Dirk Arnez <smalldirkalex@gmail.com>
2022-11-10 19:10:58 +0800

    - update Launchers/Code.cmd

--- Launchers/Code.cmd
+++ Launchers/Code.cmd
@@ -2,6 +2,7 @@
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 
 SET PATH=^
+%DOWNLOADS_DIR%;^
 %DOWNLOADS_DIR%\LLVM-13.0.0-win64\bin;^
 %DOWNLOADS_DIR%\node-v16.13.1-win-x64;^
 %DOWNLOADS_DIR%\go\bin;^
