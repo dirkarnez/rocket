5f33199a093986497622cbedd0d6e25f7ea205f9
Dirk Arnez <smalldirkalex@gmail.com>
2023-04-01 21:16:56 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -14,28 +14,24 @@ echo Unzipping node...
 echo Unzipping LLVM...
 "C:\Program Files\7-Zip\7z.exe" x LLVM-13.0.0-win64.zip -o%DOWNLOADS_DIR%\LLVM-13.0.0-win64
 
-# echo Unzipping mingw64-posix...
-# "C:\Program Files\7-Zip\7z.exe" x x86_64-8.1.0-release-posix-sjlj-rt_v6-rev0.7z -o%DOWNLOADS_DIR%
-# ren "%DOWNLOADS_DIR%\mingw64" "x86_64-8.1.0-release-posix-sjlj-rt_v6-rev0"
-
 echo Unzipping mingw64-win32...
 "C:\Program Files\7-Zip\7z.exe" x x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z -o%DOWNLOADS_DIR%
 ren "%DOWNLOADS_DIR%\mingw64" "x86_64-8.1.0-release-posix-seh-rt_v6-rev0"
 
-echo Unzipping arduino...
-"C:\Program Files\7-Zip\7z.exe" x arduino-1.8.19-windows.zip -o%DOWNLOADS_DIR%
+@REM echo Unzipping arduino...
+@REM "C:\Program Files\7-Zip\7z.exe" x arduino-1.8.19-windows.zip -o%DOWNLOADS_DIR%
 
-echo Unzipping avr-gcc...
-"C:\Program Files\7-Zip\7z.exe" x avr-gcc-11.1.0-x64-windows.zip -o%DOWNLOADS_DIR%
+@REM echo Unzipping avr-gcc...
+@REM "C:\Program Files\7-Zip\7z.exe" x avr-gcc-11.1.0-x64-windows.zip -o%DOWNLOADS_DIR%
 
-echo Unzipping nasm...
-"C:\Program Files\7-Zip\7z.exe" x nasm-2.15.05-win64.zip -o%DOWNLOADS_DIR%
+@REM echo Unzipping nasm...
+@REM "C:\Program Files\7-Zip\7z.exe" x nasm-2.15.05-win64.zip -o%DOWNLOADS_DIR%
 
 echo Unzipping go...
 "C:\Program Files\7-Zip\7z.exe" x go1.17.5.windows-amd64.zip -o%DOWNLOADS_DIR%
 
-# echo Unzipping curl...
-# "C:\Program Files\7-Zip\7z.exe" x curl-7.82.0_4-win64-mingw.zip -o%DOWNLOADS_DIR%
+@REM echo Unzipping curl...
+@REM "C:\Program Files\7-Zip\7z.exe" x curl-7.82.0_4-win64-mingw.zip -o%DOWNLOADS_DIR%
 
 cd ..
 
@@ -51,18 +47,17 @@ curl.exe https://github.com/dirkarnez/serial-locate/releases/download/v1.0.0/ser
 curl.exe https://github.com/dirkarnez/staticserver/releases/download/v1.2.1/staticserver-v1.2.1.zip -L -O -J
 "C:\Program Files\7-Zip\7z.exe" x staticserver-v1.2.1.zip
 
-# curl.exe https://github.com/dirkarnez/git-commit-helper/releases/download/v0.0.4/git-commit-helper-v0.0.4.zip -L -O -J
-# "C:\Program Files\7-Zip\7z.exe" x git-commit-helper-v0.0.4.zip
+@REM curl.exe https://github.com/dirkarnez/git-commit-helper/releases/download/v0.0.4/git-commit-helper-v0.0.4.zip -L -O -J
+@REM "C:\Program Files\7-Zip\7z.exe" x git-commit-helper-v0.0.4.zip
 
 curl.exe https://github.com/Kitware/CMake/releases/download/v3.26.1/cmake-3.26.1-windows-x86_64.zip -L -O -J
-echo Unzipping cmake...
 "C:\Program Files\7-Zip\7z.exe" x cmake-3.26.1-windows-x86_64.zip -o%DOWNLOADS_DIR%
-
-curl.exe https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/PortableGit-2.40.0-64-bit.7z.exe -L -O -J
-"C:\Program Files\7-Zip\7z.exe" x PortableGit-2.40.0-64-bit.7z.exe -o%DOWNLOADS_DIR%\PortableGit
 
 curl.exe https://github.com/brechtsanders/winlibs_mingw/releases/download/12.2.0-15.0.7-10.0.0-ucrt-r4/winlibs-x86_64-posix-seh-gcc-12.2.0-mingw-w64ucrt-10.0.0-r4.zip -L -O -J
 "C:\Program Files\7-Zip\7z.exe" x winlibs-x86_64-posix-seh-gcc-12.2.0-mingw-w64ucrt-10.0.0-r4.zip -o%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-12.2.0-mingw-w64ucrt-10.0.0-r4
+
+curl.exe https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/PortableGit-2.40.0-64-bit.7z.exe -L -O -J
+"C:\Program Files\7-Zip\7z.exe" x PortableGit-2.40.0-64-bit.7z.exe -o%DOWNLOADS_DIR%\PortableGit
 
 echo Copying Launchers...
 copy Launchers\Code.cmd %DOWNLOADS_DIR%\VSCode-win32-x64-1.66.1
@@ -70,6 +65,8 @@ copy Launchers\cmake-gui.cmd %DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin
 copy Launchers\git-cmd.cmd %DOWNLOADS_DIR%\PortableGit
 
 echo Setting up git config...
+SET PATH=%DOWNLOADS_DIR%\PortableGit\bin;
+
 cd /d %USERPROFILE%
 
 git config --global user.name "dirkarnez"
