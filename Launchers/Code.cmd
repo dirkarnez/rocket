f2f6d8a6e766de12f83693f64b7f3661ad083e2d
Dirk Arnez <smalldirkalex@gmail.com>
2023-05-29 12:08:59 +0800

    - update Launchers/Code.cmd

--- Launchers/Code.cmd
+++ Launchers/Code.cmd
@@ -9,7 +9,8 @@ SET PATH=^
 %DOWNLOADS_DIR%\PortableGit\bin;^
 %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
 %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
-%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;
+%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;^
+%SystemRoot%\System32
 
 REM set EXTENSION_PATH="%~dp0extensions"
 REM set USER_DATA_PATH="%~dp0user_data"
