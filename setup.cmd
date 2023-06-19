de5b99eb0f220703094f0dff1d9a5a9aa65f5ad9
Dirk Arnez <smalldirkalex@gmail.com>
2023-06-19 10:26:36 -0700

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -8,9 +8,6 @@ cd Software
 echo Unzipping VSCode...
 "C:\Program Files\7-Zip\7z.exe" x VSCode-win32-x64-1.66.1.zip -o%DOWNLOADS_DIR%\VSCode-win32-x64-1.66.1
 
-echo Unzipping node...
-"C:\Program Files\7-Zip\7z.exe" x node-v16.13.1-win-x64.zip -o%DOWNLOADS_DIR%
-
 @REM echo Unzipping LLVM...
 @REM "C:\Program Files\7-Zip\7z.exe" x LLVM-13.0.0-win64.zip -o%DOWNLOADS_DIR%\LLVM-13.0.0-win64
 
@@ -57,6 +54,9 @@ curl.exe https://github.com/brechtsanders/winlibs_mingw/releases/download/12.2.0
 
 curl.exe https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/PortableGit-2.40.0-64-bit.7z.exe -L -O -J
 "C:\Program Files\7-Zip\7z.exe" x PortableGit-2.40.0-64-bit.7z.exe -o%DOWNLOADS_DIR%\PortableGit
+
+curl.exe https://nodejs.org/dist/v16.13.1/node-v16.13.1-win-x64.zip -L -O -J
+"C:\Program Files\7-Zip\7z.exe" x node-v16.13.1-win-x64.zip -o%DOWNLOADS_DIR%
 
 for /f "tokens=*" %%a in (
 	'%DOWNLOADS_DIR%\PortableGit\usr\bin\head.exe  -n 1  %~dp0github_token.txt'
