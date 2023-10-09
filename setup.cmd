fef3dbfb4bf24383ff49cbdfdf219df1f35cecb7
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-09 16:39:23 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -34,32 +34,38 @@ echo Unzipping VSCode...
 cd ..
 copy Launchers\Code.cmd %DOWNLOADS_DIR%\VSCode-win32-x64-1.66.1
 
-cd /d %DOWNLOADS_DIR%
 @REM  -L, --location      Follow redirects
 @REM  -J, --remote-header-name Use the header-provided filename
 @REM  -O, --remote-name   Write output to a file named as the remote file
-curl.exe https://github.com/dirkarnez/serial-locate/releases/download/v1.0.0/serial-locate-v1.0.0.zip -L -O -J
-"C:\Program Files\7-Zip\7z.exe" x serial-locate-v1.0.0.zip &&^
+
+
+cd /d "%TEMP%" && ^
+%SystemRoot%\System32\curl.exe https://github.com/dirkarnez/serial-locate/releases/download/v1.0.0/serial-locate-v1.0.0.zip -L -O -J
+C:\PROGRA~1\7-Zip\7z.exe x serial-locate-v1.0.0.zip -o%DOWNLOADS_DIR% &&^
 del serial-locate-v1.0.0.zip
 
-curl.exe https://github.com/dirkarnez/staticserver/releases/download/v1.2.1/staticserver-v1.2.1.zip -L -O -J
-"C:\Program Files\7-Zip\7z.exe" x staticserver-v1.2.1.zip &&^
+cd /d "%TEMP%" && ^
+%SystemRoot%\System32\curl.exe https://github.com/dirkarnez/staticserver/releases/download/v1.2.1/staticserver-v1.2.1.zip -L -O -J
+C:\PROGRA~1\7-Zip\7z.exe x staticserver-v1.2.1.zip -o%DOWNLOADS_DIR% &&^
 del staticserver-v1.2.1.zip
 
 @REM curl.exe https://github.com/dirkarnez/git-commit-helper/releases/download/v0.0.4/git-commit-helper-v0.0.4.zip -L -O -J
 @REM "C:\Program Files\7-Zip\7z.exe" x git-commit-helper-v0.0.4.zip
 
-curl.exe https://github.com/Kitware/CMake/releases/download/v3.26.1/cmake-3.26.1-windows-x86_64.zip -L -O -J
-"C:\Program Files\7-Zip\7z.exe" x cmake-3.26.1-windows-x86_64.zip -o%DOWNLOADS_DIR% &&^
+cd /d "%TEMP%" && ^
+%SystemRoot%\System32\curl.exe https://github.com/Kitware/CMake/releases/download/v3.26.1/cmake-3.26.1-windows-x86_64.zip -L -O -J
+C:\PROGRA~1\7-Zip\7z.exe x cmake-3.26.1-windows-x86_64.zip -o%DOWNLOADS_DIR% &&^
 del cmake-3.26.1-windows-x86_64.zip
 
-curl.exe https://github.com/brechtsanders/winlibs_mingw/releases/download/11.2.0-12.0.1-9.0.0-r1/winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1.zip -L -O -J && ^
-"C:\Program Files\7-Zip\7z.exe" x winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1.zip -o%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1 &&^
+cd /d "%TEMP%" && ^
+%SystemRoot%\System32\curl.exe https://github.com/brechtsanders/winlibs_mingw/releases/download/11.2.0-12.0.1-9.0.0-r1/winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1.zip -L -O -J && ^
+C:\PROGRA~1\7-Zip\7z.exe x winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1.zip -o%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1 &&^
 del winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1.zip
 
-curl.exe https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/PortableGit-2.40.0-64-bit.7z.exe -L -O -J
-"C:\Program Files\7-Zip\7z.exe" x PortableGit-2.40.0-64-bit.7z.exe -o%DOWNLOADS_DIR%\PortableGit  && ^
-del PortableGit-2.40.0-64-bit.7z.exe
+cd /d "%TEMP%" && ^
+%SystemRoot%\System32\curl.exe https://github.com/git-for-windows/git/releases/download/v2.42.0.windows.2/PortableGit-2.42.0.2-64-bit.7z.exe -L -O -J
+C:\PROGRA~1\7-Zip\7z.exe x PortableGit-2.42.0.2-64-bit.7z.exe -o%DOWNLOADS_DIR%\PortableGit  && ^
+del PortableGit-2.42.0.2-64-bit.7z.exe
 
 cd /d "%TEMP%" && ^
 %SystemRoot%\System32\curl.exe "https://github.com/dirkarnez/cpp-tools/raw/main/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z" -L -O -J && ^
